d76384fff7a7fd21a1919c99afb868849d2997c8
TheBighi <thebighix@gmail.com>
2026-01-09 11:28:24 +0200

    NEW. 4. Komponendi jaotamine mitmeks komponendiks

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,18 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate.jsx'
 
 function ExpenseItem(props){
+    console.log(props)
     return (
+        <>
         <div className='expense-item'>
-            <div>{props.data.date.toString()}</div>
+            <ExpenseDate data={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
             </div>
         </div>
+        </>
     )
 }
 
