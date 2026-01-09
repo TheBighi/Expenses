eb9224dff3c25368d58eb6034eade00f7748953c
TheBighi <thebighix@gmail.com>
2026-01-09 11:16:20 +0200

    NEW. 3. Komponendi sisustamine andmetega

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,31 @@ import './App.css'
 import ExpenseItem from './components/ExpenseItem.jsx'
 
 function App() {
+  const expenses = 
+  [
+    {
+      date : new Date(2026, 0, 10),
+      title : 'New Book So Cool To Read',
+      price : 25.99
+    },
+    {
+      date : new Date(2026, 0, 5),
+      title : 'Fortnite V-Bucks',
+      price : 99.99
+    },
+    {
+      date : new Date(2026, 0, 6),
+      title : 'Meal',
+      price : 12.59
+    },
+
+  ]
+
   return (
     <>
-      <ExpenseItem/>
-      <ExpenseItem/>
-      <ExpenseItem/>
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
+      <ExpenseItem data={expenses[2]}/>
     </>
   )
 }
