a301c4208a5cf44bac502711a8eaad39c4f98a80
TheBighi <thebighix@gmail.com>
2026-01-15 14:16:10 +0200

    8. Ülesanne 3 - ümbritseva komponendi rakendamine teistes komponentides

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,18 +1,17 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate.jsx'
+import Card from './card.jsx'
 
 function ExpenseItem(props){
     console.log(props)
     return (
-        <>
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate data={props.data.date}/>
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}€</div>
             </div>
-        </div>
-        </>
+        </Card>
     )
 }
 
